ea0a12352dca06774133ad67d6be90829cde746e
geazyskay <younoussaabdourhaman@gmail.com>
2023-10-10 23:10:16 +0000

    adding homepage in the emotiondetector app

--- Emotion_Detection/urls.py
+++ Emotion_Detection/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('authentification.urls'))
+    path('', include('authentification.urls')),
+    path('', include('emotiondetector.urls')),
 ]
--- authentification/urls.py
+++ authentification/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
+    # path("", views.homepage, name="homepage"),
     path("login", views.login_request, name="login"),
     path("logout", views.logout_request, name= "logout"),
     path("register", views.register_request, name="register")
