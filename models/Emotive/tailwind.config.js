8263741986ac35062e77985ff1b53edf77913554
geazyskay <younoussaabdourhaman@gmail.com>
2023-10-11 01:45:58 +0000

    adding UI

---
+++ models/Emotive/tailwind.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: [
+//       "index.html",
+//     ],
+//     theme: {
+      
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
+  
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+  }
