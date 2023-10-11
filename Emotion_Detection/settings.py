fd8ea67f8d7d5456c90a877734b494285e5e95d6
geazyskay <younoussaabdourhaman@gmail.com>
2023-10-11 11:35:07 +0000

    styling

--- Emotion_Detection/settings.py
+++ Emotion_Detection/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'emotiondetector',
     'authentification',
     'crispy_forms',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
