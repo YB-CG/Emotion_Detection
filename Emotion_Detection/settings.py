fdc5108b638f2bd6ae2e8caaade8f3a3a8b5e5f9
geazyskay <younoussaabdourhaman@gmail.com>
2023-11-08 13:05:40 +0000

    Heroku settings

--- Emotion_Detection/settings.py
+++ Emotion_Detection/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,13 +124,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+# Define the URL prefix for static files
 STATIC_URL = '/static/'
+# settings.py
 
-import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
-#    ]
+
+# Define the root directory where your static files are stored
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
