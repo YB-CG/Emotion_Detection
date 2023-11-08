0b4d7b0d7b53d178fe7a4736319d0ef11e0d3724
geazyskay <younoussaabdourhaman@gmail.com>
2023-11-08 13:11:28 +0000

    setting heroku 2

--- Emotion_Detection/settings.py
+++ Emotion_Detection/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6o-l%8b@go0^xvsup-eu(1p+5nz#-!rk+a2(rr)ii1kq59n9j*'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["emotive.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Emotion_Detection.urls'
